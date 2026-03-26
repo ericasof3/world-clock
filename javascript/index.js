92dd31c659733d4e18742393341c6a0bef58b726
ericasof3 <erica.lemos3799@gmail.com>
2026-03-26 16:20:52 +0000

    Added the carousel

--- javascript/index.js
+++ javascript/index.js
@@ -1,81 +1,134 @@
-function updateTime() {
-  // Lisbon
-  let lisbonElement = document.querySelector("#lisbon");
-  if (lisbonElement) {
-    let lisbonDateElement = lisbonElement.querySelector(".date");
-    let lisbonTimeElement = lisbonElement.querySelector(".time");
-    let lisbonTime = moment().tz("Europe/Lisbon");
+let selectedCities = [];
+let currentPosition = 0;
+let visibleCities = 4;
 
-    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
-    lisbonTimeElement.innerHTML = lisbonTime.format(
-      "h:mm:ss [<small>]A[</small>]",
-    );
-  }
-
-  //Maputo
-  let maputoElement = document.querySelector("#maputo");
-  if (maputoElement) {
-    let maputoDateElement = maputoElement.querySelector(".date");
-    let maputoTimeElement = maputoElement.querySelector(".time");
-    let maputoTime = moment().tz("Africa/Maputo");
-
-    maputoDateElement.innerHTML = maputoTime.format("MMMM Do YYYY");
-    maputoTimeElement.innerHTML = maputoTime.format(
-      "h:mm:ss [<small>]A[</small>]",
-    );
-  }
-
-  //Havana
-  let havanaElement = document.querySelector("#havana");
-  if (havanaElement) {
-    let havanaDateElement = havanaElement.querySelector(".date");
-    let havanaTimeElement = havanaElement.querySelector(".time");
-    let havanaTime = moment().tz("America/Havana");
-
-    havanaDateElement.innerHTML = havanaTime.format("MMMM Do YYYY");
-    havanaTimeElement.innerHTML = havanaTime.format(
-      "h:mm:ss [<small>]A[</small>]",
-    );
-  }
-
-  //Tokyo
-  let tokyoElement = document.querySelector("#tokyo");
-  if (tokyoElement) {
-    let tokyoDateElement = tokyoElement.querySelector(".date");
-    let tokyoTimeElement = tokyoElement.querySelector(".time");
-    let tokyoTime = moment().tz("Asia/Tokyo");
-
-    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-    tokyoTimeElement.innerHTML = tokyoTime.format(
-      "h:mm:ss [<small>]A[</small>]",
-    );
-  }
+function formatCityName(cityTimeZone) {
+  return cityTimeZone.replace("_", " ").split("/")[1];
 }
 
-function updateCity(event) {
+function displayCities() {
+  let citiesElement = document.querySelector("#cities");
+  let emptyMessageElement = document.querySelector("#empty-message");
+  let citiesHTML = "";
+
+  if (selectedCities.length === 0) {
+    citiesElement.innerHTML = "";
+    citiesElement.style.transform = "translateX(0)";
+    emptyMessageElement.style.display = "flex";
+    document.querySelector("#previous-button").disabled = true;
+    document.querySelector("#next-button").disabled = true;
+    return;
+  }
+
+  emptyMessageElement.style.display = "none";
+
+  selectedCities.forEach(function (cityTimeZone) {
+    let cityName = formatCityName(cityTimeZone);
+    let cityTime = moment().tz(cityTimeZone);
+
+    let cityHTML = `
+      <div class="city">
+        <div class="city-content">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        </div>
+      </div>
+    `;
+
+    citiesHTML += cityHTML;
+  });
+
+  citiesElement.innerHTML = citiesHTML;
+
+  updateCarousel();
+  updateButtons();
+}
+
+function addCity(event) {
   let cityTimeZone = event.target.value;
+
+  if (cityTimeZone === "") {
+    return;
+  }
+
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-  let cityName = cityTimeZone.split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-    </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-      "A",
-    )}</small></div>
-  </div>
-  <a href="/">All cities</a>
-  `;
+
+  if (selectedCities.includes(cityTimeZone) === false) {
+    selectedCities.push(cityTimeZone);
+  }
+
+  if (selectedCities.length > visibleCities) {
+    currentPosition = selectedCities.length - visibleCities;
+  }
+
+  displayCities();
+  event.target.value = "";
 }
 
-updateTime();
-setInterval(updateTime, 1000);
+function updateTimes() {
+  displayCities();
+}
+
+function updateCarousel() {
+  let citiesElement = document.querySelector("#cities");
+  let movePercentage = currentPosition * 25;
+
+  citiesElement.style.transform = `translateX(-${movePercentage}%)`;
+}
+
+function showPreviousCity() {
+  if (currentPosition > 0) {
+    currentPosition = currentPosition - 1;
+    updateCarousel();
+    updateButtons();
+  }
+}
+
+function showNextCity() {
+  let lastPossiblePosition = selectedCities.length - visibleCities;
+
+  if (currentPosition < lastPossiblePosition) {
+    currentPosition = currentPosition + 1;
+    updateCarousel();
+    updateButtons();
+  }
+}
+
+function updateButtons() {
+  let previousButton = document.querySelector("#previous-button");
+  let nextButton = document.querySelector("#next-button");
+  let lastPossiblePosition = selectedCities.length - visibleCities;
+
+  if (lastPossiblePosition < 0) {
+    lastPossiblePosition = 0;
+  }
+
+  if (currentPosition === 0) {
+    previousButton.disabled = true;
+  } else {
+    previousButton.disabled = false;
+  }
+
+  if (currentPosition >= lastPossiblePosition) {
+    nextButton.disabled = true;
+  } else {
+    nextButton.disabled = false;
+  }
+}
 
 let citiesSelectElement = document.querySelector("#city");
-citiesSelectElement.addEventListener("change", updateCity);
+citiesSelectElement.addEventListener("change", addCity);
+
+let previousButton = document.querySelector("#previous-button");
+previousButton.addEventListener("click", showPreviousCity);
+
+let nextButton = document.querySelector("#next-button");
+nextButton.addEventListener("click", showNextCity);
+
+displayCities();
+setInterval(updateTime, 1000);
